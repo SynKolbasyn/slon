04a52c3faed72009c585f63ab731f2b9ae08fb38
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2024-02-14 22:19:50 +0800

    feat: small changes

--- catkin_ws/src/slon/src/camera_detection.cpp
+++ catkin_ws/src/slon/src/camera_detection.cpp
@@ -5,8 +5,15 @@
 #include <opencv2/opencv.hpp>
 #include "aruco_samples_utility.hpp"
 
+#include "ros/ros.h"
+#include "slon/qrcode.h"
+
 
 int main(int argc, char** argv) {
+    ros::init(argc, argv, "camera");
+    ros::NodeHandle n;
+    ros::Publisher qr_code_pos_pub = n.advertise<slon::qrcode>("qr_code_pos", 1000);
+
     float marker_length = 0.175;
 
     cv::aruco::DetectorParameters detectorParams;
@@ -14,7 +21,7 @@ int main(int argc, char** argv) {
     cv::aruco::ArucoDetector detector(dictionary, detectorParams);
 
     cv::Mat cam_matrix, dist_coeffs;
-    if (!readCameraParameters("camera_calibration_data.yaml", cam_matrix, dist_coeffs)) {
+    if (!readCameraParameters("camera_calibration_data_640_480.yaml", cam_matrix, dist_coeffs)) {
         std::cerr << "[ ERROR ] -> can not read camera calibration data" << std::endl;
         return -1;
     }
@@ -22,8 +29,11 @@ int main(int argc, char** argv) {
     cv::VideoCapture video(0);
     if (!video.isOpened()) {
         std::cerr << "[ ERROR ] -> can not open video" << std::endl;
+        video.release();
         return -1;
     }
+    video.set(cv::CAP_PROP_FRAME_WIDTH, 640);
+	video.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
 
     // Set coordinate system
     cv::Mat object_points(4, 1, CV_32FC3);
@@ -32,10 +42,14 @@ int main(int argc, char** argv) {
     object_points.ptr<cv::Vec3f>(0)[2] = cv::Vec3f(marker_length / 2.0, -marker_length / 2.0, 0);
     object_points.ptr<cv::Vec3f>(0)[3] = cv::Vec3f(-marker_length / 2.0, -marker_length / 2.0, 0);
 
-    while (cv::waitKey(1) != 27) {
+    ros::Rate loop_rate(10);
+
+    while (ros::ok()) {
         cv::Mat frame, gray;
         if (!video.read(frame)) {
             std::cerr << "[ ERROR ] -> can not read frame" << std::endl;
+            video.release();
+            cv::destroyAllWindows();
             return -1;
         }
         cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
@@ -58,11 +72,21 @@ int main(int argc, char** argv) {
             cv::aruco::drawDetectedMarkers(frame, corners, ids);
             for (uint64_t i = 0; i < ids.size(); ++i) {
                 cv::drawFrameAxes(frame, cam_matrix, dist_coeffs, rvecs[i], tvecs[i], marker_length, 2);
-                std::cout << rvecs[i] << "\t" << tvecs[i] << std::endl;
+                slon::qrcode qr_pos;
+                qr_pos.x = tvecs[i][0];
+                qr_pos.y = tvecs[i][1];
+                qr_pos.dist = tvecs[i][2] * 100;
+                qr_code_pos_pub.publish(qr_pos);
+                ROS_INFO("(%f, %f) -> %f", qr_pos.x, qr_pos.y, qr_pos.dist);
             }
         }
 
         imshow("aruco detection", frame);
+
+        if (cv::waitKey(1) == 27) break;
+
+        ros::spinOnce();
+		loop_rate.sleep();
     }
 
     video.release();
