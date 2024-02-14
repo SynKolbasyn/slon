bd1e7c9973b374c4a0abc3fddcc4cef8cbc8ea1d
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2024-02-14 21:59:33 +0800

    feat: Improved performance

--- catkin_ws/src/slon/src/camera_detection.cpp
+++ catkin_ws/src/slon/src/camera_detection.cpp
@@ -1,103 +1,72 @@
-#include <opencv2/highgui.hpp>
-#include <opencv2/aruco.hpp>
 #include <iostream>
+#include <vector>
+#include <cstdint>
+
+#include <opencv2/opencv.hpp>
 #include "aruco_samples_utility.hpp"
 
-using namespace std;
-using namespace cv;
 
+int main(int argc, char** argv) {
+    float marker_length = 0.175;
 
-int main(int argc, char *argv[]) {/*
-    CommandLineParser parser(argc, argv, keys);
-    parser.about(about);
+    cv::aruco::DetectorParameters detectorParams;
+    cv::aruco::Dictionary dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
+    cv::aruco::ArucoDetector detector(dictionary, detectorParams);
 
-    if(argc < 2) {
-        parser.printMessage();
-        return 0;
-    }*/
-
-    bool showRejected = false;
-    bool estimatePose = true;
-    float markerLength = 0.176;
-
-    aruco::DetectorParameters detectorParams;
-
-    std::cout << "Corner refinement method (0: None, 1: Subpixel, 2:contour, 3: AprilTag 2): " << (int)detectorParams.cornerRefinementMethod << std::endl;
-
-    int camId = 0;
-
-    aruco::Dictionary dictionary = aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
-
-    Mat camMatrix, distCoeffs;
-    if(estimatePose) {
-        bool readOk = readCameraParameters(parser.get<string>("c"), camMatrix, distCoeffs);
-        if(!readOk) {
-            cerr << "Invalid camera file" << endl;
-            return 0;
-        }
+    cv::Mat cam_matrix, dist_coeffs;
+    if (!readCameraParameters("camera_calibration_data.yaml", cam_matrix, dist_coeffs)) {
+        std::cerr << "[ ERROR ] -> can not read camera calibration data" << std::endl;
+        return -1;
     }
-    aruco::ArucoDetector detector(dictionary, detectorParams);
-    VideoCapture inputVideo(camId);
-    int waitTime = 1;
 
-    double totalTime = 0;
-    int totalIterations = 0;
+    cv::VideoCapture video(0);
+    if (!video.isOpened()) {
+        std::cerr << "[ ERROR ] -> can not open video" << std::endl;
+        return -1;
+    }
 
     // Set coordinate system
-    cv::Mat objPoints(4, 1, CV_32FC3);
-    objPoints.ptr<Vec3f>(0)[0] = Vec3f(-markerLength/2.f, markerLength/2.f, 0);
-    objPoints.ptr<Vec3f>(0)[1] = Vec3f(markerLength/2.f, markerLength/2.f, 0);
-    objPoints.ptr<Vec3f>(0)[2] = Vec3f(markerLength/2.f, -markerLength/2.f, 0);
-    objPoints.ptr<Vec3f>(0)[3] = Vec3f(-markerLength/2.f, -markerLength/2.f, 0);
+    cv::Mat object_points(4, 1, CV_32FC3);
+    object_points.ptr<cv::Vec3f>(0)[0] = cv::Vec3f(-marker_length / 2.0, marker_length / 2.0, 0);
+    object_points.ptr<cv::Vec3f>(0)[1] = cv::Vec3f(marker_length / 2.0, marker_length / 2.0, 0);
+    object_points.ptr<cv::Vec3f>(0)[2] = cv::Vec3f(marker_length / 2.0, -marker_length / 2.0, 0);
+    object_points.ptr<cv::Vec3f>(0)[3] = cv::Vec3f(-marker_length / 2.0, -marker_length / 2.0, 0);
 
-    while(inputVideo.grab()) {
-        Mat image, imageCopy;
-        inputVideo.retrieve(image);
+    while (cv::waitKey(1) != 27) {
+        cv::Mat frame, gray;
+        if (!video.read(frame)) {
+            std::cerr << "[ ERROR ] -> can not read frame" << std::endl;
+            return -1;
+        }
+        cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
 
-        double tick = (double)getTickCount();
-
-        vector< int > ids;
-        vector< vector< Point2f > > corners, rejected;
+        std::vector<int32_t> ids;
+        std::vector<std::vector<cv::Point2f>> corners, rejected;
 
         // detect markers and estimate pose
-        detector.detectMarkers(image, corners, ids, rejected);
+        detector.detectMarkers(gray, corners, ids, rejected);
 
-        size_t  nMarkers = corners.size();
-        vector<Vec3d> rvecs(nMarkers), tvecs(nMarkers);
+        std::vector<cv::Vec3d> rvecs(corners.size()), tvecs(corners.size());
 
-        if(estimatePose && !ids.empty()) {
+        if (!ids.empty()) {
             // Calculate pose for each marker
-            for (size_t  i = 0; i < nMarkers; i++) {
-                solvePnP(objPoints, corners.at(i), camMatrix, distCoeffs, rvecs.at(i), tvecs.at(i));
-            }
-        }
-
-        double currentTime = ((double)getTickCount() - tick) / getTickFrequency();
-        totalTime += currentTime;
-        totalIterations++;
-        if(totalIterations % 30 == 0) {
-            cout << "Detection Time = " << currentTime * 1000 << " ms "
-                 << "(Mean = " << 1000 * totalTime / double(totalIterations) << " ms)" << endl;
+            for (uint64_t  i = 0; i < corners.size(); ++i) cv::solvePnP(object_points, corners.at(i), cam_matrix, dist_coeffs, rvecs.at(i), tvecs.at(i));
         }
 
         // draw results
-        image.copyTo(imageCopy);
-        if(!ids.empty()) {
-            aruco::drawDetectedMarkers(imageCopy, corners, ids);
-
-            if(estimatePose) {
-                for(unsigned int i = 0; i < ids.size(); i++)
-                    cv::drawFrameAxes(imageCopy, camMatrix, distCoeffs, rvecs[i], tvecs[i], markerLength * 1.5f, 2);
+        if (!ids.empty()) {
+            cv::aruco::drawDetectedMarkers(frame, corners, ids);
+            for (uint64_t i = 0; i < ids.size(); ++i) {
+                cv::drawFrameAxes(frame, cam_matrix, dist_coeffs, rvecs[i], tvecs[i], marker_length, 2);
+                std::cout << rvecs[i] << "\t" << tvecs[i] << std::endl;
             }
         }
 
-        if(showRejected && !rejected.empty())
-            aruco::drawDetectedMarkers(imageCopy, rejected, noArray(), Scalar(100, 0, 255));
-
-        imshow("out", imageCopy);
-        char key = (char)waitKey(waitTime);
-        if(key == 27) break;
+        imshow("aruco detection", frame);
     }
+
+    video.release();
+    cv::destroyAllWindows();
 
     return 0;
 }
